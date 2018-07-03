03e4ebb6ce857b173aa80ba1cec27981203aa9ea
金敏洁 <1223515114@qq.com>
2018-07-03 12:54:38 +0800

    金  shopping Json  首页和商城数据

--- project/src/index.js
+++ project/src/index.js
@@ -1,19 +1,19 @@
 /*BASE*/
 import React from 'react';
-import ReactDOM, {render} from 'react-dom';
-import {HashRouter,Redirect,Switch,Route} from 'react-router-dom';
-import {Provider} from "react-redux"
-import store from './store/index'
+// import ReactDOM, {render} from 'react-dom';
+// import {HashRouter,Redirect,Switch,Route} from 'react-router-dom';
+// import {Provider} from "react-redux"
+// import store from './store/index'
 import axios from './api/index';
 import md5 from 'blueimp-md5';
 
 // antd
-import {LocaleProvider} from "antd";
-import zh_CN from "antd/lib/locale-provider/zh_CN";
+// import {LocaleProvider} from "antd";
+// import zh_CN from "antd/lib/locale-provider/zh_CN";
 
 // import CSS
-import './static/css/reset.min.css';
-import "./static/css/common.less";
+// import './static/css/reset.min.css';
+// import "./static/css/common.less";
 
 
 //导入组公共组件
@@ -23,13 +23,13 @@ import "./static/css/common.less";
 // import Mycourse from "./routes/Mycourse";
 // import Person from "./routes/Person";
 
-render(<Provider store={store}>
-    <HashRouter>
-        <LocaleProvider locale={zh_CN}>
-            <div>
-               
-               
-            </div>
-        </LocaleProvider>
-    </HashRouter>
-</Provider>,root);
+// render(<Provider store={store}>
+//     <HashRouter>
+//         <LocaleProvider locale={zh_CN}>
+//             <div>
+//
+//
+//             </div>
+//         </LocaleProvider>
+//     </HashRouter>
+// </Provider>,root);
--- project/src/routes/Home.js
+++ project/src/routes/Home.js
@@ -12,7 +12,7 @@ class Home extends React.Component{
         return <section >
             <Switch>
              
-                // <Route path={'/course/info'} component={Info}></Route>
+                {/*<Route path={'/course/info'} component={Info}></Route>*/}
             </Switch>
         </section>
     }
